14e95511198e9e02bb32702ee798c7d0d1a6d778
alexgrab577 <alexgrab@cox.net>
2016-03-24 13:44:55 -0700

    Addnames

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var currentName = "";
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    socket.emit('chat', msg);
+    var message = 't: ' + msg;
+    console.log('message: ' + message);
+    socket.emit('chat', message + 'peppers');
+  });
+  socket.on('add name', function(name){
+    console.log('nameAdd: ' + name);
+    currentName = name;
+
+    console.log('nameAdd2: ' + currentName);
   });
   //socket.emit('chat', "test");
 });
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 //server.listen(80);  //listen on port 80
 
+var currentName = '';
 
 var routes = require('./routes');
 
@@ -16,7 +17,15 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    socket.emit('chat', msg);
+    io.emit('chat', currentName + ': ' + msg);
+  });
+    socket.on('add name', function(name){
+    currentName = name;
+    console.log('adding name: ' + currentName);
+    io.emit('newUser', currentName + ' connected to chat');
+  });
+   socket.on('disconnect', function(){
+    io.emit('newUser', currentName + ' disconnected from chat');
   });
   //socket.emit('chat', "test");
 });
